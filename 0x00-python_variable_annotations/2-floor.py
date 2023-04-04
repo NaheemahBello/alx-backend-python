664be8f13198c251d5370dd85daa47f25cc5aaed
NaheemahBello <nimabello009@gmail.com>
2023-04-04 11:08:08 +0100

    2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Module for Task 2.
+'''
+
+
+def floor(n: float) -> int:
+    '''Computes the greatest integer less than or equal to a floating-point number.
+    '''
+    return int(n)
