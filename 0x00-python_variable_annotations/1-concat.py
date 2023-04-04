d626ac720c5c3bdc25a9e4c3b07ba0c0835517ee
NaheemahBello <nimabello009@gmail.com>
2023-04-04 11:02:32 +0100

    task 1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -3,7 +3,7 @@
 '''
 
 
-def concatenate_strings(str1: str, str2: str) -> str:
+def concat(str1: str, str2: str) -> str:
     '''Joins two strings together into a single string.
     '''
     return str1 + str2
